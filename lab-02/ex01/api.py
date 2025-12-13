e2b0c0ca4c064b7d0368a0c1f73d7077f0a70658
AkaUdeptrai <duy.dat2002@gmail.com>
2025-12-14 01:18:01 +0700

    [add] lab-02 transposition

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from cipher.caesar import CaesarCipher
+from cipher.playfair import PlayFairCipher
 
 app = Flask(__name__)
 
@@ -37,6 +38,34 @@ def caesar_decrypt():
         return jsonify({"error": "key must be an integer"}), 400
     decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({"decrypted_message": decrypted_text})
+
+#PLAYFAIR CIPHER ALGORITHM
+playfair_cipher = PlayFairCipher()
+
+@app.route('/api/playfair/creatematrix', methods=['POST'])
+def playfair_creatematrix():
+    data = request.json
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    return jsonify({"playfair_matrix": playfair_matrix})
+
+@app.route('/api/playfair/encrypt', methods=['POST'])
+def playfair_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(plain_text, playfair_matrix)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/playfair/decrypt', methods=['POST'])
+def playfair_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(cipher_text, playfair_matrix)
+    return jsonify({'decrypted_text': decrypted_text})
 
 # main function
 if __name__ == "__main__":
